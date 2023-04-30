2753e87a7801e849ae8225cad1404f6066b83b7b
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-01 02:38:43 +0600

    PrivateRouter updated

--- src/PrivateRouter/PrivateRouter.jsx
+++ src/PrivateRouter/PrivateRouter.jsx
@@ -1,14 +1,20 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
 
-const PrivateRouter = ({children}) => {
-    const{user} = useContext(AuthContext)
+const PrivateRouter = ({ children }) => {
+    const { user, loading } = useContext(AuthContext)
     const location = useLocation()
-    if(user){
+
+    if (loading) {
+        return <Spinner animation="border" variant="danger" />
+    }
+
+    if (user) {
         return children
     }
-    return <Navigate state={{from:location}} to='/layout/login' replace></Navigate>
+    return <Navigate state={{ from: location }} to='/layout/login' replace></Navigate>
 };
 
 export default PrivateRouter;
