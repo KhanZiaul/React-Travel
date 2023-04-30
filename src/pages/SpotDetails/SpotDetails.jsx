508d816708510a5b556030ed470bbc97ed0f976d
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-30 12:43:01 +0600

    spottitle and  details added

--- src/pages/SpotDetails/SpotDetails.jsx
+++ src/pages/SpotDetails/SpotDetails.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
+import { useLoaderData } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 const SpotDetails = () => {
+    const spotDetails = useLoaderData()
     return (
         <div className='bgImage'>
             <Navigation></Navigation>
+            <Container className='mt-5'>
+                <div>
+                    <h1 className='text-light'>{spotDetails.name}</h1>
+                    <p className='text-light'>{spotDetails.details}</p>
+                </div>
+            </Container>
         </div>
     );
 };
