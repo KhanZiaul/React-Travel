52c470f3578710e9daa86d81d3ce3f4701879ae4
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-05 20:41:21 +0600

    Contact Us section done

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            <h2>This is contact page</h2>
+        <div className='p-5'>
+            <div className='border rounded border-primary p-5'>
+                <h2> Contact Us</h2>
+                <p>
+                    We'd love to hear from you! If you have any questions, comments, or feedback about our site, please don't hesitate to get in touch. Here are a few ways to reach us:</p>
+
+                <p> Email: info@travelGuru .com <br />
+                    Phone: 1-800-123-4567 <br />
+                    Address: Road # 01, Sadek Khan Rd, Dhaka 1207 <br />
+                    Bangladesh</p>
+
+                <p>Our customer service team is available 24/7 to assist you with any issues you may have. You can also connect with us on social media:</p>
+
+                <p>
+                    Facebook: @travelGuru <br />
+                    Twitter: @travelGuru <br />
+                    Instagram: @travelGuru <br />
+                </p>
+                <p>
+                    We look forward to hearing from you and helping you plan your next adventure!</p>
+            </div>
         </div>
     );
 };
